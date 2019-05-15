4cfde3bfb8fd5db2f9683163ba83c2fd8d228d6f
John Desrosiers <john.desro@gmail.com>
2019-05-15 07:33:50 -0700

    Add quick sort

--- quick-sort.js
+++ quick-sort.js
@@ -1,38 +1,47 @@
-// function quickSort(arr) {
-//   if (arr.length < 2) return arr
-//   const pivotValue = arr[0]
-//   let j = 1
-//   for (let i = 1; i < arr.length; i += 1) {
-//     if (arr[i] < pivotValue) {
-//       swap(arr, i, j)
-//       j += 1
-//     }
-//   }
-//   const left = quickSort(arr.slice(1, j))
-//   left.push(pivotValue)
-//   const right = quickSort(arr.slice(j))
-//   return [...left, ...right]
-// }
-
-// function swap(arr, i, j) {
-//   const temp = arr[i]
-//   arr[i] = arr[j]
-//   arr[j] = temp
-// }
-
-function quickSort(arr, startAt = 0, endAt = arr.length - 1) {
-  if (startAt < endAt) {
-    const pivotIndex = pivot(arr, startAt, endAt)
-    quickSort(arr, startAt, pivotIndex - 1)
-    quickSort(arr, pivotIndex + 1, endAt)
+// iterative version is only limited by the maximum size
+// of an array in javascript
+function quickSort(arr) {
+  const partitions = [createPartition(0, arr.length - 1)]
+  let part, pivotAt
+  while (partitions.length) {
+    part = partitions.pop()
+    pivotAt = pivot(arr, part.start, part.end)
+    if (pivotAt > part.start) {
+      partitions.push(createPartition(part.start, pivotAt - 1))
+    }
+    if (pivotAt < part.end) {
+      partitions.push(createPartition(pivotAt + 1, part.end))
+    }
   }
   return arr
+
+  function createPartition(start, end) {
+    return { start, end }
+  }
 }
-const numbers = [2, 13, 4, 9, 1]
-console.log(numbers)
-const res = quickSort(numbers)
-console.log(numbers)
-console.log(res)
+
+// recursive solution (max call stack on my desktop is hit when array contains
+// 125361 elements)
+// function quickSort(arr, startAt = 0, endAt = arr.length - 1) {
+//   if (startAt < endAt) {
+//     const pivotIndex = pivot(arr, startAt, endAt)
+//     quickSort(arr, startAt, pivotIndex - 1)
+//     quickSort(arr, pivotIndex + 1, endAt)
+//   }
+//   return arr
+// }
+
+// function* generateRandomInt(size) {
+//   for (let counter = 0; counter < size; counter += 1) {
+//     yield Math.floor(Math.random() * size)
+//   }
+// }
+// const largeArray = Array.from(generateRandomInt(1000000))
+// const numbers = [2, 13, 4, 9, 1]
+
+// console.log(largeArray ? largeArray.slice(0, 30) : numbers)
+// const res = quickSort(largeArray || numbers)
+// console.log(largeArray ? largeArray.slice(0, 30) : numbers)
 
 function pivot(arr, startIndex = 0, endIndex = arr.length - 1) {
   const pivotValue = arr[startIndex]
