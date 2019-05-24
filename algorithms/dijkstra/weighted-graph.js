fdbda2eac7c29d2b44c83100ff60915c48401358
John Desrosiers <john.desro@gmail.com>
2019-05-24 04:57:45 -0700

    Initial WeightedGraph class (dijkstra)

---
+++ algorithms/dijkstra/weighted-graph.js
@@ -0,0 +1,27 @@
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(name) {
+    if (!this.adjacencyList[name]) {
+      this.adjacencyList[name] = new Vertex()
+    }
+  }
+
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push(new Edge(v2, weight))
+    this.adjacencyList[v2].push(new Edge(v1, weight))
+  }
+}
+
+class Vertex extends Array {}
+
+class Edge {
+  constructor(name, weight) {
+    this.name = name
+    this.weight = weight
+  }
+}
+
+module.exports = { WeightedGraph }
