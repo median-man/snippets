c920119fdcd51a99d6f891573d1f9e9fc6c73a0f
John Desrosiers <john.desro@gmail.com>
2019-05-24 05:08:22 -0700

    Initial PriorityQueue class (dijkstras)

---
+++ algorithms/dijkstra/priority-queue.js
@@ -0,0 +1,27 @@
+/* 
+  Slow implementation using array and sorting for each enqueue call - O(nlogn)
+*/
+class PriorityQueue {
+  constructor() {
+    this.elements = []
+  }
+  enqueue(value, priority) {
+    this.elements.push(new Element(value, priority))
+    this._sort()
+  }
+  dequeue() {
+    return this.elements.shift()
+  }
+  _sort() {
+    return this.values.sort((a, b) => a.priority - b.priority)
+  }
+}
+
+class Element {
+  constructor(value, priority) {
+    this.value = value
+    this.priority = priority
+  }
+}
+
+module.exports = { PriorityQueue }
